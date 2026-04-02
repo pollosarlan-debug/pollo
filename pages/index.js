187c8580ebb09fa9b5df2420072585e453a6c2eb
pollosarlan-debug <pollosarlan@gmail.com>
2026-04-02 16:59:29 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{background: "black", color: "white", height: "100vh"}}>
+      <h1>Pollo</h1>
+      <p>Editor de videos</p>
+    </div>
+  );
+}
